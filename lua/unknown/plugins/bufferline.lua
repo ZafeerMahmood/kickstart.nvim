6a933333b6d76448d4decc82afd590eeddf8776c
ZafeerMahmood <zafeer7460@gmail.com>
2026-01-31 03:21:55 +0500

    feat: add leader+X to close all code buffers

--- lua/unknown/plugins/bufferline.lua
+++ lua/unknown/plugins/bufferline.lua
@@ -37,5 +37,27 @@ return {
       vim.cmd('BufferLineCyclePrev')
       vim.cmd('bdelete ' .. buf_to_close)
     end, { desc = 'Close buffer' })
+
+    vim.keymap.set('n', '<leader>X', function()
+      local exclude_ft = { 'neo-tree', 'terminal', 'lazygit', 'fugitive', 'gitcommit', 'diff', 'claudecode' }
+      local exclude_bt = { 'terminal', 'nofile', 'prompt', 'quickfix' }
+      local bufs_to_close = {}
+      for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+        if vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].buflisted then
+          local ft = vim.bo[buf].filetype
+          local bt = vim.bo[buf].buftype
+          local name = vim.api.nvim_buf_get_name(buf)
+          if name ~= '' and not vim.tbl_contains(exclude_ft, ft) and not vim.tbl_contains(exclude_bt, bt) then
+            table.insert(bufs_to_close, buf)
+          end
+        end
+      end
+      if #bufs_to_close > 0 then
+        vim.cmd('enew')
+        for _, buf in ipairs(bufs_to_close) do
+          pcall(vim.cmd, 'bdelete ' .. buf)
+        end
+      end
+    end, { desc = 'Close all code buffers' })
   end,
 }
