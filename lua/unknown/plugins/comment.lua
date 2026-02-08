27579eb6cfb893402003f7432aed5459b640d735
ZafeerMahmood <zafeer7460@gmail.com>
2026-02-08 05:55:55 +0500

    comment to work with tsx and jsx files

--- lua/unknown/plugins/comment.lua
+++ lua/unknown/plugins/comment.lua
@@ -1,8 +1,15 @@
 -- Toggle comments with gcc (line) or gc{motion}
--- See: https://github.com/numToStr/Comment.nvim
+-- Uses ts-context-commentstring for JSX/TSX support ({/* */} inside JSX)
 
 return {
   'numToStr/Comment.nvim',
   event = 'VeryLazy',
-  opts = {},
+  dependencies = {
+    'JoosepAlviste/nvim-ts-context-commentstring',
+  },
+  opts = function()
+    return {
+      pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+    }
+  end,
 }
